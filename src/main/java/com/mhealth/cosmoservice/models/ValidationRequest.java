f1f0f8ec5a6ae206bcf0a38d4c348327cfc2f1e1
Conor <conorloughran99@gmail.com>
2023-03-07 23:22:07 +0000

    Testing

---
+++ src/main/java/com/mhealth/cosmoservice/models/ValidationRequest.java
@@ -0,0 +1,10 @@
+package main.java.com.mhealth.cosmoservice.models;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class ValidationRequest {
+    private boolean isValidatorConnected;
+}
--- src/main/java/com/mhealth/cosmoservice/repositories/GameSessionRepository.java
+++ src/main/java/com/mhealth/cosmoservice/repositories/GameSessionRepository.java
@@ -2,5 +2,7 @@ package main.java.com.mhealth.cosmoservice.repositories;
 
 import com.azure.spring.data.cosmos.repository.ReactiveCosmosRepository;
 import main.java.com.mhealth.cosmoservice.models.GameSessionModel;
+import reactor.core.publisher.Mono;
 
-public interface GameSessionRepository extends ReactiveCosmosRepository<GameSessionModel, String> {}
+public interface GameSessionRepository extends ReactiveCosmosRepository<GameSessionModel, String> {
+}
